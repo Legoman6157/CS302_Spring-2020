2cfb0cf6d0d4ef3357d02b69a1948a6d0bfbd373
Jash Byers <jbyers@localhost.localdomain>
2020-03-05 17:49:13 -0500

    Added challenge 6 test files

--- challenges/chal6/test/adjacency/adjacency.cpp
+++ challenges/chal6/test/adjacency/adjacency.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -5,7 +6,7 @@
 #include "matrix.h"
 
 int main() {
-	Matrix m;
+	Matrix<int> *m;
 
 	std::stringstream ss;
 	std::string line_buffer;
@@ -14,16 +15,29 @@ int main() {
 	int N_elements;
 
 	while (std::cin >> N_elements) {
+		std::cout << N_elements << std::endl;
+		m = new Matrix<int>(N_elements, N_elements);
 
-		m.resize(N_elements, N_elements);
+		std::cin.ignore();
+
 
 		for (int i = 0; i < N_elements; i++) {
 			std::getline(std::cin, line_buffer);
+
+			ss.clear();
 			ss.str(line_buffer);
 			for (int j = 0; j < N_elements; j++) {
 				ss >> num_buff;
-				m[i][j] = num_buff;
+				(*m)[i][j] = num_buff;
 			}//for j < N_elements
 		}
+
+		for (int i = 0; i < m->N_rows; i++) {
+			for (int j = 0; j < m->N_cols; j++)
+				std::cout << (*m)[i][j] << ' ';
+			std::cout << std::endl;
+		}
+
+		delete m;
 	}//while (std::cin >> N_elements)
 }//main
--- challenges/chal6/test/adjacency/matrix.h
+++ challenges/chal6/test/adjacency/matrix.h
@@ -1,6 +1,7 @@
 template <typename T>
 class Matrix {
-	private:
+//	private:
+	public:
 		T **matrix;
 
 	public:
@@ -8,6 +9,12 @@ class Matrix {
 			 N_cols,
 			 N;
 
+		Matrix() {
+			N_rows = 0;
+			N_cols = 0;
+			N = 0;
+			matrix = NULL;
+		}
 		Matrix(int n_N_rows, int n_N_cols): N_rows(n_N_rows), N_cols(n_N_cols) {
 			matrix = new T* [N_rows];
 
@@ -16,17 +23,21 @@ class Matrix {
 		}//Matrix(int n_N_rows, int n_N_cols)
 
 		~Matrix() {
-			for (int i = 0; i < N_rows; i++)
-				delete matrix[i];
-
-			delete [] matrix;
+			if (matrix) {
+				for (int i = 0; i < N_rows; i++)
+					delete matrix[i];
+	
+				delete [] matrix;
+			}
 		}//~Matrix
 
 		void resize(int n_N_rows, int n_N_cols) {
-			for (int i = 0; i < N_rows; i++)
-				delete matrix[i];
-
-			delete [] matrix;
+			if (matrix) {
+				for (int i = 0; i < N_rows; i++)
+					delete matrix[i];
+	
+				delete [] matrix;
+			}
 
 			matrix = new T* [N_rows];
 
