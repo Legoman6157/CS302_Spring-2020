97b4b04934bf50e4bd47d4e69d3c108ab4e4922d
Joshua <jbyers3@vols.utk.edu>
2020-03-10 09:13:57 -0400

    Gameboard now gets board from input file

--- projects/proj4/src/Gameboard.cpp
+++ projects/proj4/src/Gameboard.cpp
@@ -3,6 +3,10 @@
 void Gameboard::read_board() {
 	int N_pieces;
 
+	std::stringstream ss;
+	std::string line_buff;
+	char char_buff;
+
 	char piece_buff;
 	int cost_buff;
 
@@ -13,6 +17,20 @@ void Gameboard::read_board() {
 		piece_costs.insert(std::pair<char, int>(piece_buff, cost_buff));
 	}
 
-	for (auto it : piece_costs)
-		std::cout << it.first << ' ' << it.second << std::endl;
+	std::cin >> N_rows >> N_cols;
+	board.resize(N_rows, N_cols);
+
+	for (int i = 0; i < N_rows; i++) {
+		for (int j = 0; j < N_cols; j++) {
+			std::cin >> char_buff;
+			board[i][j] = char_buff;
+		}// for (j < N_cols)
+	}//for (i < N_rows)
+
+	std::cout << "Printing board:" << std::endl;
+	for (int i = 0; i < N_rows; i++) {
+		for (int j = 0; j < N_cols; j++)
+			std::cout << board[i][j];
+		std::cout << std::endl;
+	}
 }//read_board
