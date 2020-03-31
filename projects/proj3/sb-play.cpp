e6ee00915067d1057ca334c370485ff5d02e65b5
Joshua <jbyers3@vols.utk.edu>
2020-03-31 17:50:57 -0400

    Over 180

--- projects/proj3/sb-play.cpp
+++ projects/proj3/sb-play.cpp
@@ -149,7 +149,10 @@ std::vector<dset> sb_analyze(Superball *s) {
 
 	//Variables for the current cell and its neighbors
 	int curr_pixel_loc,
-		 dir_loc[4];
+		 north_loc,
+		 east_loc,
+		 west_loc,
+		 south_loc;
 
 	//For all goal pieces,
 	for (int i = 0; i < goal_pieces.size(); i++) {
@@ -171,31 +174,87 @@ std::vector<dset> sb_analyze(Superball *s) {
 			//Set location of current pixel it neighbors
 			//Set location of current pixel and possible "movements" for traversal
 			curr_pixel_loc = (curr_pixel.x) * s->c + (curr_pixel.y);
-			dir_loc[0] = (curr_pixel.x-1) * s->c + (curr_pixel.y);
-			dir_loc[1] = (curr_pixel.x) * s->c + (curr_pixel.y+1);
-			dir_loc[2] = (curr_pixel.x) * s->c + (curr_pixel.y-1);
-			dir_loc[3] = (curr_pixel.x+1) * s->c + (curr_pixel.y);
+			north_loc = (curr_pixel.x-1) * s->c + (curr_pixel.y);
+			east_loc = (curr_pixel.x) * s->c + (curr_pixel.y+1);
+			west_loc = (curr_pixel.x) * s->c + (curr_pixel.y-1);
+			south_loc = (curr_pixel.x+1) * s->c + (curr_pixel.y);
 
-			for (int i = 0; i < 4; i++) {
-				if ( (curr_pixel.x > 0)
-						&& (curr_pixel.y < s->c-1)
-						&& (curr_pixel.y > 0)
-						&& (curr_pixel.x < s->r-1)
-						&& (ds.Find(dir_loc[i]) != ds.Find(curr_pixel_loc))
-						&& (s->board[dir_loc[i]] == s->board[curr_pixel_loc]) ) {
+			//(N) Check North
+			//If the current pixel is below the top row,
+			//	and the north and current pixels have different parents
+			//	and the north and current piece match,
+			if ( (curr_pixel.x > 0)
+					&& (ds.Find(north_loc) != ds.Find(curr_pixel_loc))
+					&& (s->board[north_loc] == s->board[curr_pixel_loc]) ) {
 
-					//Match them
-					ds.Union(ds.Find(dir_loc[i]), ds.Find(curr_pixel_loc));
+				//Match them
+				ds.Union(ds.Find(north_loc), ds.Find(curr_pixel_loc));
 
-					//Set the temporary pixel's x and y to north's
-					temp_pixel.x = dir_loc[i]/s->c;
-					temp_pixel.y = dir_loc[i]%s->c;
+				//Set the temporary pixel's x and y to north's
+				temp_pixel.x = north_loc/s->c;
+				temp_pixel.y = north_loc%s->c;
 
-					dfs_stack.push(temp_pixel);
-					dfs_stack.push(pixel(temp_pixel));
-					temp_dset.pixels.push_back(temp_pixel);
-				}
+				dfs_stack.push(temp_pixel);
+				temp_dset.pixels.push_back(temp_pixel);
 			}
+
+			//(E) Check East
+			//If the current pixel is below the top row,
+			//	and the east and current pixels have different parents
+			//	and the east and current piece match,
+			if ( (curr_pixel.y < s->c-1)
+					&& (ds.Find(east_loc) != ds.Find(curr_pixel_loc))
+					&& (s->board[east_loc] == s->board[curr_pixel_loc]) ) {
+
+				//Match them
+				ds.Union(ds.Find(east_loc), ds.Find(curr_pixel_loc));
+
+				//Set the temporary pixel's x and y to east's
+				temp_pixel.x = east_loc/s->c;
+				temp_pixel.y = east_loc%s->c;
+
+				dfs_stack.push(temp_pixel);
+				temp_dset.pixels.push_back(temp_pixel);
+			}
+
+			//(W) Check West
+			//If the current pixel is below the top row,
+			//	and the west and current pixels have different parents
+			//	and the west and current piece match,
+			if ( (curr_pixel.y > 0)
+					&& (ds.Find(west_loc) != ds.Find(curr_pixel_loc))
+					&& (s->board[west_loc] == s->board[curr_pixel_loc]) ) {
+
+				//Match them
+				ds.Union(ds.Find(west_loc), ds.Find(curr_pixel_loc));
+
+				//Set the temporary pixel's x and y to west's
+				temp_pixel.x = west_loc/s->c;
+				temp_pixel.y = west_loc%s->c;
+
+				dfs_stack.push(temp_pixel);
+				temp_dset.pixels.push_back(temp_pixel);
+			}
+
+			//(S) Check South
+			//If the current pixel is below the top row,
+			//	and the south and current pixels have different parents
+			//	and the south and current piece match,
+			if ( (curr_pixel.x < s->r-1)
+					&& (ds.Find(south_loc) != ds.Find(curr_pixel_loc))
+					&& (s->board[south_loc] == s->board[curr_pixel_loc]) ) {
+
+				//Match them
+				ds.Union(ds.Find(south_loc), ds.Find(curr_pixel_loc));
+
+				//Set the temporary pixel's x and y to south's
+				temp_pixel.x = south_loc/s->c;
+				temp_pixel.y = south_loc%s->c;
+
+				dfs_stack.push(temp_pixel);
+				temp_dset.pixels.push_back(temp_pixel);
+			}
+
 		}//while (!dfs_stack.empty())
 
 		curr_pixel_loc = (temp_dset.pixels[0].x) * s->c + (temp_dset.pixels[0].y);
@@ -203,7 +262,7 @@ std::vector<dset> sb_analyze(Superball *s) {
 		//This little section is to make sure I don't push a size 1 dset with a goal piece that's
 		//	in another dset.
 		int temp_pixel_loc = (temp_dset.pixels[0].x) * s->c + (temp_dset.pixels[0].y);
-		if (curr_pixel_loc == ds.Find(curr_pixel_loc) && temp_dset.pixels.size() >= 0) {
+		if (curr_pixel_loc == ds.Find(curr_pixel_loc) && temp_dset.pixels.size() >= 2) {
 			temp_dset.score = s->colors[temp_dset.piece] * temp_dset.pixels.size();
 			dsets.push_back(temp_dset);
 		}
@@ -325,6 +384,26 @@ dset get_best_dset(std::vector<dset> dsets, Superball *s) {
 
 }
 
+dset get_best_score_dset(std::vector<dset> dsets, Superball *s) {
+	bool valid_score_dset_found = false;
+
+	dset temp;
+
+	std::sort(dsets.begin(), dsets.end());
+	int i = dsets.size();
+
+	do {
+		i--;
+		temp = dsets[i];
+
+		for (int j = 0; j < temp.pixels.size() && !valid_score_dset_found; j++) {
+			valid_score_dset_found = s->goals[ (temp.pixels[j].x) * (s->c) + (temp.pixels[j].y) ];
+		}
+	} while (!valid_score_dset_found && i > 0);
+
+	return temp;
+}
+
 pixel get_swap_piece(dset main, Superball *s) {
 
 	int N = s->r*s->c;
@@ -401,9 +480,9 @@ int main(int argc, char **argv)
 		swap_random_pieces(s);
 		o_f << "Swap random pieces";
 	} else {
+		dset biggest_dset = get_best_score_dset(dsets, s);
 
-		if (s->empty < 5) {
-			dset biggest_dset = get_biggest_dset(dsets);
+		if (biggest_dset.pixels.size() >= s->mss) {
 
 			int i = 0;
 			while (i < biggest_dset.pixels.size() && (s->goals[(biggest_dset.pixels[i].x) * s->c + biggest_dset.pixels[i].y] != 1)) {i++;}
@@ -412,8 +491,7 @@ int main(int argc, char **argv)
 			for (auto it : biggest_dset.pixels)
 				o_f << it.x << ' ' << it.y << '\n';
 
-//			if (i == biggest_dset.pixels.size()-1 && (s->goals[(biggest_dset.pixels[i].x) * s->c + biggest_dset.pixels[i].y] != 1))
-			if (biggest_dset.pixels[i].y == 0)
+			if (i == biggest_dset.pixels.size()-1 && (s->goals[(biggest_dset.pixels[i].x) * s->c + biggest_dset.pixels[i].y] != 1))
 				swap_random_pieces(s);
 			else
 				score(biggest_dset.pixels[i]);
@@ -443,6 +521,8 @@ int main(int argc, char **argv)
 		}//if (s->empty < 20)
 
 	}//if (dsets.size() == 0) / else
+
+	int i;
 
 	o_f.close();
 
